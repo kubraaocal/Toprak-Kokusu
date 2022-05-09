9c325d1a4395aa9daa0b84b2146d302d07895adc
kubraaocal <kubraaocall@gmail.com>
2022-05-09 18:44:24 +0300

    Kamp yeri harita eklendi.

--- app/src/main/java/com/example/toprakkokusu/CampCreateActivity.java
+++ app/src/main/java/com/example/toprakkokusu/CampCreateActivity.java
@@ -2,9 +2,14 @@ package com.example.toprakkokusu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,13 +18,26 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class CampCreateActivity extends AppCompatActivity implements View.OnClickListener{
+
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
 
     private FirebaseAuth mAuth;
     private DatabaseReference cDbRef;
@@ -28,10 +46,15 @@ public class CampCreateActivity extends AppCompatActivity implements View.OnClic
     private Button btnSave;
     private EditText editTextExplanation,editTextCampName;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camp_create);
+
+        supportMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+
+        client= LocationServices.getFusedLocationProviderClient(this);
 
         mAuth=FirebaseAuth.getInstance();
         cDbRef= FirebaseDatabase.getInstance().getReference().child("Camp");
@@ -50,6 +73,49 @@ public class CampCreateActivity extends AppCompatActivity implements View.OnClic
 
         btnSave=findViewById(R.id.btnSave);
         btnSave.setOnClickListener(this);
+
+        if(ActivityCompat.checkSelfPermission(CampCreateActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            getCurrentLocation();
+        }else{
+            ActivityCompat.requestPermissions(CampCreateActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},44);
+        }
+    }
+
+    private void getCurrentLocation() {
+        Task<Location> task= client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!=null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            LatLng latLng =new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options=new MarkerOptions().position(latLng)
+                                    .title("Konumum");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            }
+            else{
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+
     }
 
     @Override
@@ -123,7 +189,5 @@ public class CampCreateActivity extends AppCompatActivity implements View.OnClic
                 }
             }
         });
-
-
     }
 }
